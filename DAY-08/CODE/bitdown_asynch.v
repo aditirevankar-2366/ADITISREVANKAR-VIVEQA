188ab2d8374ace73dd52fe1f8a8e6d5f383fb9f1
adytea2366 <aditishaileshrevankar@gmail.com>
2026-07-12 14:48:23 +0530

    Add asynchronous bit down counter module

---
+++ DAY-08/CODE/bitdown_asynch.v
@@ -0,0 +1,28 @@
+module bitdown_asynch(
+input clk,
+input rst,
+output reg [2:0] q
+    );
+    
+  always @(negedge clk or posedge rst)
+  begin
+  if (rst)
+  q[0] <= 1'b1;
+  else
+  q[0] <= ~q[0];
+  
+  end
+  always @(negedge q[0] or posedge rst)
+  begin 
+  if (rst)
+   q[1] <= 1'b1;
+  else
+   q[1]<=~q[1];
+  end
+ always @(negedge q[1] or posedge rst)
+ begin
+   if (rst)
+     q[2]<=1'b1;
+   else q[2]<=~q[2];
+   end
+endmodule
